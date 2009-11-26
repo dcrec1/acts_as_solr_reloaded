e2e72f1d56d5f561ddc5b030091bfe8e1cbbc1f8
Ricardo Almeida <ricardoluiz1.almeida@gmail.com>
2009-11-26 11:29:49 +0800

    generators for local and dynamic attributes

---
+++ generators/dynamic_attributes_migration/templates/migration.rb
@@ -0,0 +1,15 @@
+class DynamicAttributesMigration < ActiveRecord::Migration
+  def self.up
+    create_table :dynamic_attributes do |t|
+      t.integer :dynamicable_id
+      t.string  :dynamicable_type
+      t.string :name
+      t.text :value
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :dynamic_attributes
+  end
+end
---
+++ generators/local_migration/templates/migration.rb
@@ -0,0 +1,16 @@
+class LocalMigration < ActiveRecord::Migration
+  def self.up
+    create_table :locals do |t|
+      t.integer :localizable_id
+      t.string  :localizable_type
+      t.string :latitude
+      t.string :longitude
+
+      t.timestamps
+    end
+  end
+  
+  def self.down
+    drop_table :locals
+  end
+end
