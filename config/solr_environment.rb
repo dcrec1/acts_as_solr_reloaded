0b4aa1afb46c200fd520aab0202ad215e39854ae
Braulio Bhavamitra <brauliobo@gmail.com>
2011-05-26 18:03:53 -0300

    Add SOLR_HOST and SOLR_PID_FILE

--- config/solr_environment.rb
+++ config/solr_environment.rb
@@ -1,12 +1,14 @@
 ENV['RAILS_ENV']  = (ENV['RAILS_ENV'] || 'development').dup
-# RAILS_ROOT isn't defined yet, so figure it out.
 require "uri"
 require "fileutils"
 require "yaml"
 dir = File.dirname(__FILE__)
 SOLR_PATH = File.expand_path("#{dir}/../solr") unless defined? SOLR_PATH
 
-RAILS_ROOT = File.expand_path("#{File.dirname(__FILE__)}/../test") unless defined? RAILS_ROOT
+# RAILS_ROOT isn't defined yet, so figure it out.
+unless defined? RAILS_ROOT
+  RAILS_ROOT = File.expand_path("#{File.dirname(__FILE__)}/../test")
+end
 unless defined? SOLR_LOGS_PATH
   SOLR_LOGS_PATH = ENV["SOLR_LOGS_PATH"] || "#{RAILS_ROOT}/log"
 end
@@ -19,10 +21,15 @@ end
 unless defined? SOLR_CONFIG_PATH
   SOLR_CONFIG_PATH = ENV["SOLR_CONFIG_PATH"] || "#{SOLR_PATH}/solr"
 end
+unless defined? SOLR_PID_FILE
+  SOLR_PID_FILE="#{SOLR_PIDS_PATH}/solr.#{ENV['RAILS_ENV']}.pid"
+end
 
 unless defined? SOLR_PORT
   config = YAML::load_file(RAILS_ROOT+'/config/solr.yml')
   raise("No solr environment defined for RAILS_ENV the #{ENV['RAILS_ENV'].inspect}") unless config[ENV['RAILS_ENV']]
+
+  SOLR_HOST = ENV['HOST'] || URI.parse(config[ENV['RAILS_ENV']]['url']).host
   SOLR_PORT = ENV['PORT'] || URI.parse(config[ENV['RAILS_ENV']]['url']).port
 end
 
